c2181bfaf2c4bbe2ecd6ba850db314345f7ae0c8
yadavlalu5252 <yadavlalu5252@gmail.com>
2026-09-01 20:31:35 +0530

    Initial project setup

---
+++ LearningPlatform/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LearningPlatform.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
