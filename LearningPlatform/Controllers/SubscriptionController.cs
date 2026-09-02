a2f8a83d6d0b455129f17780a7e2b380930af9ad
Your Name <you@example.com>
2026-09-02 20:58:02 +0530

    subscription model added

--- LearningPlatform/Controllers/SubscriptionController.cs
+++ LearningPlatform/Controllers/SubscriptionController.cs
@@ -9,7 +9,7 @@ namespace LearningPlatform.Controllers
         public IActionResult Index()
         {
             return View();
-        }
+        }                                                                                                                                                                                                                                                                                                                                                                                                             
 
         public IActionResult Privacy()
         {
@@ -22,4 +22,4 @@ namespace LearningPlatform.Controllers
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
-}
+} 
---
+++ LearningPlatform/Data/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿
+
+using LearningPlatform.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LearningPlatform.Data
+{
+    public class AppDbContext : DbContext
+    {
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Subscriptioncs> Subscriptions { get; set; }
+   
+
+
+
+
+
+    }
+}
+    
