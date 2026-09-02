f293dc792e986d8536804a3b603ccf29a7f76491
Vaibhavi Girkar <vaibhavigirkar@gmail.com>
2026-09-02 20:58:50 +0530

    AddTopic Added

---
+++ LearningPlatform/Controllers/AddTopicController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LearningPlatform.Controllers
+{
+    public class AddTopicController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- LearningPlatform/Program.cs
+++ LearningPlatform/Program.cs
@@ -1,7 +1,16 @@
+using LearningPlatform.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+
+
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<AppDbContext>(
+    options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConn")));
 
 var app = builder.Build();
 
